bd24b54e43c79a4f8336e72c404e76daaf4c3aa9
2208504 <tofi1115@colorado.edu>
2024-04-01 16:18:26 -0600

    Added Classes corresponding to each control loop

---
+++ Primary_Flight_Code/secondary_flight_code/Hbridge.cpp
@@ -0,0 +1,32 @@
+/* H-Bridge code */
+#include "Hbridge.h"
+
+Hbridge::Hbridge(byte pwm1, byte pwm2) {
+  this->pwm1 = pwm1;
+  this->pwm2 = pwm2;
+  init();
+}
+
+// Set up Hbridge pinns
+void Hbridge::init() {
+  pinMode(pwm1, OUTPUT);
+  pinMode(pwm2, OUTPUT);
+}
+
+// heat build surface
+void Hbridge::forward() {
+  analogWrite(pwm1, 0);    // Low
+  analogWrite(pwm2, 255);  // High
+}
+
+//cool build surface
+void Hbridge::reverse() {
+  analogWrite(pwm1, 255);  // High
+  analogWrite(pwm2, 0);    // Low
+}
+
+//stop or standby mode
+void Hbridge::off() {
+  analogWrite(pwm1, 0);  // Low
+  analogWrite(pwm2, 0);  // Low
+}
---
+++ Primary_Flight_Code/secondary_flight_code/Hbridge.h
@@ -0,0 +1,24 @@
+/* H-Bridge header file */
+
+// prevents class from being defined multiple times
+#ifndef MY_HBRIDGE_H
+#define MY_HBRIDGE_H
+
+#include <Arduino.h>
+
+// defines Hbridge class
+class Hbridge {
+
+private:
+  byte pwm1;  // IN1
+  byte pwm2;  // IN2
+
+public:
+  Hbridge(byte pwm1, byte pwm2);
+  void init();
+  void forward();
+  void reverse();
+  void off();
+};
+
+#endif
