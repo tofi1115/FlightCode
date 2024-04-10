5c6efd3ddc213f16a67a67f02ef4e9e36e8eaafd
2208504 <tofi1115@colorado.edu>
2024-04-09 20:56:54 -0600

    Added Break_Coast_Test

--- Test_Code/Break_Cost_Test/Break_Cost_Test.ino
+++ Test_Code/Break_Cost_Test/Break_Cost_Test.ino
@@ -1,9 +1,30 @@
+/* Motor Hardware Test Code */
+/* Test Motor Off vs Break Mode */
+
+  int motor_pin1 = ;
+  int motor_pin2 = ;
+
 void setup() {
-  // put your setup code here, to run once:
+  //
+  pinMode(motor_pin1, OUTPUT);
+  pinMode(motor_pin2,OUTPUT);
+
+  analogWrite(motor_pin1, 0);
+  analogWrite(motor_pin2, 0);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //put motor into Break Mode
+  analogWrite(motor_pin1, 255);
+  analogWrite(motor_pin2,255);
+  delay(1000);
+
+  //put motor into Coast Mode
+  analogWrite(motor_pin1, 255);
+  analogWrite(motor_pin2,255);
+  delay(1000);  
+
 
 }
