cf59c8e54ada2fbc708feaf2db1063e0ed5e9da6
2208504 <tofi1115@colorado.edu>
2024-04-09 19:59:06 -0600

    Set Up Test Code Folders

---
+++ Test_Code/Forward_Backward_Test/Forward_Backward_Test.ino
@@ -0,0 +1,20 @@
+/* Motor Hardware Test Code */
+
+void setup() {
+  // Motor Pin Variables
+  int motor_pin1;
+  int motor_pin2;
+
+  //
+  pinMode(motor_pin1, OUTPUT);
+  pinMode(motor_pin2,OUTPUT);
+
+}
+
+void loop() {
+
+
+  
+  
+
+}
