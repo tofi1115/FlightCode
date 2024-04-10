30d935bdbb4972212faa0ffead7a81b9f22784a2
2208504 <tofi1115@colorado.edu>
2024-04-09 20:56:38 -0600

    Fixed Forward_Backward_Test

--- Test_Code/Forward_Backward_Test/Forward_Backward_Test.ino
+++ Test_Code/Forward_Backward_Test/Forward_Backward_Test.ino
@@ -1,22 +1,41 @@
 /* Motor Hardware Test Code */
+/* Test Directions Motors Turn*/
 
-void setup() {
-  // Motor Pin Variables
   int motor_pin1 = ;
   int motor_pin2 = ;
+
+//When Testing, Indicate the Directions of Motor Rotation In this Code
+
+void setup() {
 
   //
   pinMode(motor_pin1, OUTPUT);
   pinMode(motor_pin2,OUTPUT);
 
+  analogWrite(motor_pin1, 0);
+  analogWrite(motor_pin2, 0);
+
+
 }
 
 void loop() {
-
-  digitalWrite(motor_pin1, HIGH); //Go Direction 1
+  //Go Direction 1
+  analogWrite(motor_pin1, 255);
+  analogWrite(motor_pin2, 0);
   delay(1000);
 
-  digitalWrite(motor_pin2, HIGH); //Go Direction 2
+  //Stop
+  analogWrite(motor_pin1, 0);
+  analogWrite(motor_pin2, 0);
   delay(1000);
 
+  //Go Directionl 2
+  analogWrite(motor_pin1, 0);
+  analogWrite(motor_pin2, 255);
+  delay(1000);
+
+  //Stop
+  analogWrite(motor_pin1, 0);
+  analogWrite(motor_pin2, 0);
+  delay(1000);
 }
