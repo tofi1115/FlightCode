347e8d77c03742cebf65716d795f3517615fabb3
2208504 <tofi1115@colorado.edu>
2024-04-09 21:19:46 -0600

    Wrote Photoresistor_Test

--- Test_Code/Photoresistor_Test/Photoresistor_Test.ino
+++ Test_Code/Photoresistor_Test/Photoresistor_Test.ino
@@ -1,9 +1,34 @@
+/* Photoresistor Hardware Test Code */
+
+  //Photoresistor Input Values
+  const int A0 = ;
+  const int A1 = ;
+
+  //Values of Photoresistors
+  int value1;
+  int value2;
+
 void setup() {
-  // put your setup code here, to run once:
+
+  // set pin modes
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  Serial.println("Value1  Value2");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  // read photoresistor values
+  value1 = analogRead(A0);
+  value2 = analogRead(A1);
+
+  //Print results to Serial Monitor
+  Serial.println(Value1, "  ", Value2);
+
+
+
+  
+
 
 }
