c0b800dd0c50db247cfd65ec13dcac768329a9e9
2208504 <tofi1115@colorado.edu>
2024-04-12 01:07:06 -0600

    Wrote Basic Control Loop

--- Flight_Code/secondary_flight_code/secondary_flight_code.ino
+++ Flight_Code/secondary_flight_code/secondary_flight_code.ino
@@ -3,8 +3,7 @@
 # include "Hbridge.h"
 # include "Photoresistor.h"
 
-  // Pin Constants
-      // potentially change to data type byte if this doesn't work
+//Constants
 
       //Right Motor
   int rightMotor_pin1 = 6 ;
@@ -14,16 +13,21 @@
   int leftMotor_pin1 = 3 ;
   int leftMotor_pin2 = 5 ;
 
+      //Photoresistor Pins
   byte rightPhoto_pin = A1;
   byte leftPhoto_pin = A0;
 
-  int Photoresistor_Resistance = 50; // Used for calculating actual Photoresistor value
-  int DelayStart= 120000;
+  int DelayStart= 120000; //Delay before turning Motors on
+  byte Photoresistor_Resistance=50; //Photoresistor associated resistance|Not used, but required for class defenitions
 
-  float RightBrightness;
-  float LeftBrightness;
-  float RightOverLeftRatio;
-  float fudgeFactor = .1;
+  float RightBrightness; //Brightness read by Right Photoresistor
+  float LeftBrightness;  //Brightness read by left Photoresistor
+  float RightOverLeftRatio; //Ratio of right brightness over left
+  float fudgeFactor = .1; //
+  float MinBrightness = 15; //Minimum required brightness to trigger control loop
+  int loopDelay = 100;
+
+  uint32_t timeStamp = 0;
 
   Hbridge rightMotor(rightMotor_pin1, rightMotor_pin2);
   Hbridge leftMotor(leftMotor_pin1, leftMotor_pin2);
@@ -33,7 +37,13 @@
 
 void setup() {
   // put your setup code here, to run once:
+
+  rightMotor.off();
+  leftMotor.off();
+
   Serial.begin(9600);
+  //delay(DelayStart);
+
 }
 
 void loop() {
@@ -42,27 +52,36 @@ RightBrightness= rightPhoto.CheckValue()+1;
 LeftBrightness=leftPhoto.CheckValue()+1;
 
 RightOverLeftRatio=RightBrightness/LeftBrightness;
-if (RightBrightness>100){
-if (RightOverLeftRatio>1+fudgeFactor) {
-  //Motors to turn Right
-  Serial.print("Right");
-  rightMotor.forward();
-  leftMotor.backward();
 
-} else if(RightOverLeftRatio<1-fudgeFactor) {
-  //Motors to turn Left
-  Serial.print("Left");
-  rightMotor.backward();
-  leftMotor.forward();
+if (RightBrightness>MinBrightness){
+  if (RightOverLeftRatio>1+fudgeFactor) {
+    //Motors to turn Right
+    rightMotor.forward();
+    leftMotor.backward();
 
-} else {
-  rightMotor.hold();
-  leftMotor.hold();
-}
-} else {
-  rightMotor.forward();
-  leftMotor.backward();
-}
+  } else if(RightOverLeftRatio<1-fudgeFactor) {
+    //Motors to turn Left
+    rightMotor.backward();
+    leftMotor.forward();
 
+  } else {
+    rightMotor.hold();
+    leftMotor.hold();
+  }
+  } else {
+    rightMotor.forward();
+    leftMotor.backward();
+  }
+  
+  delay(loopDelay);
 
+  //Log Data
+  timeStamp = millis();
+  
+  Serial.print(timeStamp);
+  Serial.print(",");
+  Serial.print(RightBrightness);
+  Serial.print(",");
+  Serial.print(LeftBrightness);
+  Serial.println();
 }
